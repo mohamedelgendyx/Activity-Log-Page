c275fe9a64c5ca3cb2cd09fcad715805745d0f37
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:15:06 +0300

    use loading flags to disable the button

--- src/components/headerButtons.tsx
+++ src/components/headerButtons.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const HeaderButtons: React.FC<any> = () => {
+const HeaderButtons: React.FC<any> = ({ loading, loadingMore }) => {
+
     return (
         <section className="flex bg-transparent text-xs">
-            <button className={`flex justify-center items-center gap-x-1.5 px-4 py-2 uppercase border rounded-r-md hover:bg-[#e9e9e9]`}>
+            <button
+                className={`flex justify-center items-center gap-x-1.5 px-4 py-2 uppercase border rounded-r-md ${loading || loadingMore ? 'bg-gray-200 text-gray-400 cursor-not-allowed' : 'hover:bg-[#e9e9e9] '}`}
+                disabled={loading || loadingMore}
+            >
                 <img src={require('../assets/export.svg').default} />
                 export
             </button>
