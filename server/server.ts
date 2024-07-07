db6462159b884e894b65096abc77a521076d4b42
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2024-07-07 14:48:25 +0300

    only allow our own frontend domain to access the server and database

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,19 @@ import eventRoutes from './database/eventRoutes';
 const app = express();
 const port = process.env.PORT || 4321;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.ALLOWED_URL,
+    optionsSuccessStatus: 200
+}));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    const url = req.get('Origin');
+    if (!url || url !== process.env.ALLOWED_URL) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+    next();
+});
 
 // Use the event routes
 app.use('/api/events', eventRoutes);
