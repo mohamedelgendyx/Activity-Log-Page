2eb29c40eb2767e45ff314285bf82104281976a0
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:11:20 +0300

    drill loading flags to header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import SearchBar from "./searchBar";
 import HeaderButtons from "./headerButtons";
 
-const Header: React.FC<any> = () => {
+const Header: React.FC<any> = ({ loading, loadingMore }) => {
     return (
         <section className="bg-neutral-100 w-full pt-4">
             <section className="container p-4 flex text-[#575757]">
                 <SearchBar />
-                <HeaderButtons />
+                <HeaderButtons loading={loading} loadingMore={loadingMore} />
             </section>
         </section>
     );
