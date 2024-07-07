68b1d4fd45b88af84f3277acae4fe9668a373402
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:13:58 +0300

    drill loading search data to search baar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import SearchBar from "./searchBar";
 import HeaderButtons from "./headerButtons";
 
-const Header: React.FC<any> = ({ loading, loadingMore }) => {
+const Header: React.FC<any> = ({ loading, loadingMore, searchTerm, handleSearch }) => {
     return (
         <section className="bg-neutral-100 w-full pt-4">
             <section className="container p-4 flex text-[#575757]">
-                <SearchBar />
+                <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
                 <HeaderButtons loading={loading} loadingMore={loadingMore} />
             </section>
         </section>
