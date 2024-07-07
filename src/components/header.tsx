2e1c020367cb13c889a5f3fe098509c90edcd794
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:21:37 +0300

    use search handler method

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import SearchBar from "./searchBar";
 import HeaderButtons from "./headerButtons";
 
-const Header: React.FC<any> = ({ activities, loading, loadingMore, searchTerm, handleSearch }) => {
+const Header: React.FC<any> = ({ activities, loading, loadingMore, handleSearch }) => {
     return (
         <section className="bg-neutral-100 w-full pt-4">
             <section className="container p-4 flex text-[#575757]">
-                <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
+                <SearchBar handleSearch={handleSearch} />
                 <HeaderButtons activities={activities} loading={loading} loadingMore={loadingMore} />
             </section>
         </section>
--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const SearchBar: React.FC<any> = ({ searchTerm, handleSearch }) => {
+const SearchBar: React.FC<any> = ({ handleSearch }) => {
     return (
         <section className="flex-grow ">
             <input
                 type="text"
                 placeholder="Search name, email or action..."
                 className="w-full p-2 text-sm border rounded-l-lg bg-transparent focus:outline-none"
-                value={searchTerm}
                 onChange={(e) => handleSearch(e.target.value.trim())}
             />
         </section>
