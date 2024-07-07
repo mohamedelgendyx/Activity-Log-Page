2bb71b33b20fa4f2469514f9de97a978abe840fe
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2024-07-07 14:51:25 +0300

    style the detailed row section

--- src/components/ActivityDetails.tsx
+++ src/components/ActivityDetails.tsx
@@ -5,7 +5,7 @@ const DetailedActivityRow: React.FC<any> = ({ activity, expanded }) => {
     return (
         <tr>
             <td colSpan={4} className="p-0">
-                <div className={`text-[#929292] w-[calc(100%+30px)] text-sm grid grid-cols-3 gap-6 rounded-lg bg-white mx-[-15px] drop-shadow overflow-hidden transition-all duration-250 ease-in-out ${expanded ? 'max-h-80 p-6 pl-7 border' : 'max-h-0 border-0 padding-0'}`}>
+                <div className={`text-[#929292] w-[calc(100%+30px)] text-sm grid grid-cols-3 gap-6 px-6 rounded-lg bg-white mx-[-15px] drop-shadow overflow-hidden transition-all duration-250 ease-in-out ${expanded ? 'max-h-80 py-6 pl-7 border' : 'max-h-0 border-0 padding-0'}`}>
                     <DetailedActivityRowDataSection
                         headerTitle={'actor'}
                         metadata={[
