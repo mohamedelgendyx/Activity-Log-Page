d2791a50779bbbb7de79fec60a4931fe8a6e59f3
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:19:03 +0300

    init the frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ActivityLog from './pages/activityLog';
+
+function App() {
+  return (
+    <div className="App">
+      <ActivityLog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        REACT_APP_BACKEND_API_URL: string;
+        // add other env variables here
+    }
+}
