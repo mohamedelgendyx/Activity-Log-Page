478a81b70bc053071e522a53851a33e2269df9ed
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2024-07-07 05:40:21 +0300

    implement the GET request to get events data paginated and with a search and filter apply

--- server/database/eventRoutes.ts
+++ server/database/eventRoutes.ts
@@ -16,6 +16,61 @@ const generateId = (prefix: string) => {
 
 router
     .route('/')
+    .get(async (req: any, res: any) => {
+        try {
+            const {
+                page = '1',
+                limit = '5',
+                search,
+                filter,
+            } = req.query;
+
+            const pageNumber = parseInt(page as string, 10);
+            const limitNumber = parseInt(limit as string, 10);
+            const skip = (pageNumber - 1) * limitNumber;
+            let whereClause: any = {};
+
+            if (search) {
+                whereClause.OR = [
+                    { actor_id: { contains: search as string, mode: 'insensitive' } },
+                    { actor_name: { contains: search as string, mode: 'insensitive' } },
+                    { object: { contains: search as string, mode: 'insensitive' } },
+                    { group: { contains: search as string, mode: 'insensitive' } },
+                    { target_id: { contains: search as string, mode: 'insensitive' } },
+                    { target_name: { contains: search as string, mode: 'insensitive' } },
+                    { location: { contains: search as string, mode: 'insensitive' } },
+                    { action: { path: ['name'], string_contains: search as string } },
+                    { action: { path: ['object'], string_contains: search as string } },
+                    { metadata: { path: ['description'], string_contains: search as string } },
+                    { metadata: { path: ['redirect'], string_contains: search as string } }
+                ];
+            }
+
+            if (filter && filter !== '{}') {
+                const filterObj = JSON.parse(filter as string);
+                if (filterObj.actor_id) whereClause.actor_id = { contains: filterObj.actor_id };
+                if (filterObj.actor_name) whereClause.actor_name = { contains: filterObj.actor_name };
+                if (filterObj.target_id) whereClause.target_id = { contains: filterObj.target_id };
+                if (filterObj.target_name) whereClause.target_name = { contains: filterObj.target_name };
+                if (filterObj.action_id) whereClause.action = { path: ['id'], string_contains: filterObj.action_id };
+                if (filterObj.action_name) whereClause.action = { path: ['name'], string_contains: filterObj.action_name };
+            }
+
+            const totalCount = await prisma.event.count({ where: whereClause });
+            const events = await prisma.event.findMany({
+                where: whereClause,
+                skip,
+                take: limitNumber,
+                orderBy: {
+                    occurred_at: 'desc',
+                },
+            });
+            res.status(200).json({ events, totalCount });
+        } catch (error) {
+            console.log("🚀 ~ .get ~ error:", error)
+            res.status(500).json({ error: 'Unable to fetch events' });
+        }
+    })
     .post(async (req: any, res: any) => {
         try {
             const {
