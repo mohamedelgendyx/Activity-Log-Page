dd65eea188ee999d174ee22b3442d9095da7ed6b
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:05:54 +0300

    implement details row data sections

--- src/components/activityDetailsData.tsx
+++ src/components/activityDetailsData.tsx
@@ -2,17 +2,17 @@
 const DetailedActivityRowDataSection = ({ headerTitle, metadata }: any) => {
     return (
         <div>
-                <p className="mb-3 uppercase font-semibold">{headerTitle}</p>
-                {
-                    metadata.map(({ label, value }: any) => {
-                        return (
-                            <div key={`${headerTitle}-${label}`} className="grid grid-cols-[100px_1fr] gap-3 mb-2 items-center">
-                                <span className="capitalize">{label}</span>
-                                <span className="text-black">{value}</span>
-                            </div>
-                        )
-                    })
-                }
+            <p className="mb-3 uppercase font-semibold">{headerTitle}</p>
+            {
+                metadata.map(({ label, value }: any) => {
+                    return (
+                        <div key={`${headerTitle}-${label}`} className="grid grid-cols-[100px_1fr] gap-3 mb-2 items-center">
+                            <span className="capitalize">{label}</span>
+                            <span className="text-black">{value}</span>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
