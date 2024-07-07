431ced864116242e4e44013eb8ac377bfee10a64
Mohamed Elgendy <mohamed.elgendy@vultara.com>
2024-07-07 05:16:47 +0300

    drill activities list to the header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import SearchBar from "./searchBar";
 import HeaderButtons from "./headerButtons";
 
-const Header: React.FC<any> = ({ loading, loadingMore, searchTerm, handleSearch }) => {
+const Header: React.FC<any> = ({ activities, loading, loadingMore, searchTerm, handleSearch }) => {
     return (
         <section className="bg-neutral-100 w-full pt-4">
             <section className="container p-4 flex text-[#575757]">
                 <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
-                <HeaderButtons loading={loading} loadingMore={loadingMore} />
+                <HeaderButtons activities={activities} loading={loading} loadingMore={loadingMore} />
             </section>
         </section>
     );
